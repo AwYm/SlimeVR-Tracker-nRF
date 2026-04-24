627441f53265989b023abf1513fe67eda9b228d2
AwYm <53154951+AwYm@users.noreply.github.com>
2026-04-24 06:01:33 +0200

    Add sensor configuration for Promicro board in global>h

--- src/globals.h
+++ src/globals.h
@@ -70,6 +70,14 @@
 #define SENSOR_MAGNETOMETER_AXES_ALIGNMENT my, mx, mz
 #endif
 
+#if defined(CONFIG_BOARD_FOXSMOL40_UF2)
+#define SENSOR_MAGNETOMETER_AXES_ALIGNMENT my, mx, mz
+#endif
+
+#if defined(CONFIG_BOARD_PROMICRO_UF2)
+#define SENSOR_QUATERNION_CORRECTION 1.0f, 0.0f, 0.0f, 0.0f
+#endif
+
 // default orientation for most boards with the sensor mounted flat on the PCB
 // with the top side as +X and front side as +Z and left side as +Y from your perspective
 // on stacked promicro with common breakout board
